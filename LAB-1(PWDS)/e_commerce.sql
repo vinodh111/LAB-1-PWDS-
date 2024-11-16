ba583165d3a23f60bc3513f42fcc22fdb28da072
vinodh111 <vinodhsharma111@gmail.com>
2024-11-16 18:07:20 +0530

    Lab 1 assignment

---
+++ LAB-1(PWDS)/e_commerce.sql
@@ -0,0 +1,8 @@
+select * from e_commerce.category;
+select * from e_commerce.customer;
+select * from e_commerce.orders;
+select * from e_commerce.product;
+select * from e_commerce.product_details;
+select * from e_commerce.rating;
+select * from e_commerce.supplier;
+
---
+++ README.md
@@ -0,0 +1,2 @@
+"# Vinodh_Lab1_Python_for_DS" 
+![e_commerce](https://github.com/user-attachments/assets/dcec7c19-f671-4707-8780-b8fed9bf19b6)
